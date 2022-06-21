8e94f0880ff6f995c36bfe5f3aa558192aefa97e
hayan-kim <greenhurb95@naver.com>
2022-06-21 11:27:59 +0900

    multiple image upload mongodb

--- models/image.js
+++ models/image.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const imageSchema = mongoose.Schema({    
-    imageUrl: {
-        type: String,
+    imageUrls: {
+        type: Array,
         required: true,
     },
     createdAt: {
