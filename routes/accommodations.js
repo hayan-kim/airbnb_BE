b5c84ecc3161de924d89046cc0e37351bf0a1f22
alpha-fly <placetobe@naver.com>
2022-06-21 22:28:40 +0900

    전역함수에 await

--- routes/accommodations.js
+++ routes/accommodations.js
@@ -116,7 +116,7 @@ router.post("/", authMiddleware, async (req, res) => {
 
   // 예약가능기간(openAt ~ closeAt)의 각 날짜들을 key로, 예약가능여부를 value로(boolean) 갖는 "공실 객체" Vacancy 생성
   const openDays = (closeAt - openAt) / 86400000 + 1;
-  let Vacancy = createVacancy(openDays, openAt);
+  let Vacancy = await createVacancy(openDays, openAt);
   
   // for (let i = 1; i <= openDays; i++) {
   //   let humanDate = new Date(openAt + 86400000 * (i - 1));
